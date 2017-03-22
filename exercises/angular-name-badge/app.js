2d5e8456f4129adb7253e795fd1ba6eac6d05aa5
Timothy Dawson <timltdawson@gmail.com>
2017-03-22 11:06:16 -0600

    finished angular-name-badges

--- exercises/angular-name-badge/app.js
+++ exercises/angular-name-badge/app.js
@@ -1,23 +1,13 @@
 var app = angular.module("myApp", []);
 
 app.controller("myController", ["$scope", function($scope){
-	$scope.submission = function(){
-		$scope.newBadge = {
-			firstName: $scope.firstName,
-			lastName: $scope.lastName,
-			email: $scope.email,
-			birthPlace: $scope.birthPlace,
-			phone: $scope.phone,
-			food: $scope.food,
-			bio: $scope.bio
-		}
+	
+	$scope.badges = [];
+	
+	$scope.submission = function(newBadge){
+		$scope.badges.push(newBadge);
+		$scope.newBadge = {};
 	}
 	
 }])
-
-function newThingy(){
-	$(".newspot").append("<div class='well'><div class='row'><h4>Hi my name is....</h4></div><div class='row'><div class='madeInput'>{{newBadge.firstName}}</div><div class='madeInput pull-right'>{{newBadge.lastName}}</div></div><div class='row'><div class='madeInput'>{{newBadge.email}}</div><div class='madeInput pull-right'>{{newBadge.birthPlace}}</div></div><div class='row'><div class='madeInput'>{{newBadge.phone}}</div><div class='madeInput pull-right'>{{newBadge.food}}</div></div><div class='row'><div class='madeInput' id='bioo'>{{newBadge.bio}}</div></div></div>")
-}
-
-
 
--- warmups/split-by-num/app.js
+++ warmups/split-by-num/app.js
@@ -1,7 +1,6 @@
 function splitNum(arr, num){
-	var newArray = [];
 	for (var i = 0; i < arr.length; i+=num){
-		var smallArray = arr.slice(i, i+num);
+		var smallArray = arr.slice(i, i + num);
 		console.log(smallArray);
 	}
 }
@@ -13,3 +12,10 @@ splitNum([12, 3, 4, 5, 2, 1, 4, 5, 2, 3, 1, 2, 4, 5, 6, 7, 4, 2, 3, 5], 3)
 
 
 
+betterVersion = function (things, num) {
+	var output = [];
+	while (things.length) output.push(things.splice(0 , num))
+	return output;
+}
+
+console.log(betterVersion(["a", "b", "c", "d", 3, 4, 5, 6, 7, 8, 9,], 3));
